f05c2700cb66c9fba5ecf9dc5775de65dc56f603
Taylor O'Brien <toobrien@amazon.com>
2023-03-20 20:29:02 -0700

    adj q

--- auto_quote/quote_defs.py
+++ auto_quote/quote_defs.py
@@ -8,11 +8,11 @@ QUOTE_DEFS = [
         "duration":         5,
         "action":           "BUY",
         "time_in_force":    "GTD",
-        "entry":            -0.0500,
+        "entry":            -0.0050,
         "enabled":          True,
         "max_worsening":    0.0025,
-        "profit_taker_amt": 0.0050,
-        "stop_loss_amt":    -0.0050,
+        "profit_taker_amt": 0.0040,
+        "stop_loss_amt":    -0.080,
         "time_zone":        "US/Eastern"
     },
     {
@@ -22,11 +22,11 @@ QUOTE_DEFS = [
         "duration":         5,
         "action":           "SELL",
         "time_in_force":    "GTD",
-        "entry":            0.0500,
+        "entry":            0.0050,
         "enabled":          False,
         "max_worsening":    -0.0025,
-        "profit_taker_amt": -0.0050,
-        "stop_loss_amt":    0.0050,
+        "profit_taker_amt": -0.0040,
+        "stop_loss_amt":    0.0080,
         "time_zone":        "US/Eastern"
     },
 ]
