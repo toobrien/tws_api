0aa1090ac174496423885d08b9e0af51ad4671a4
Taylor O'Brien <toobrien@amazon.com>
2023-03-21 17:00:03 -0700

    fixed order status handler in fclient

--- l1_chart/instruments.py
+++ l1_chart/instruments.py
@@ -9,10 +9,5 @@ INSTRUMENTS = [
     ( "HO", "NYMEX", "fly",  "M23", "U23", "Z23" ),
     ( "NG", "NYMEX", "single", "J23" ),
     ( "NG", "NYMEX", "fly",  "V23", "X23", "Z23" ),
-    ( "NG", "NYMEX", "rcal",  "M23", "Z23" ),
-    ( "NG", "NYMEX", "rcal",  "X23", "H24" ),
-    ( "ZT", "CBOT", "rcal", "M23", "U23" ),
-    ( "ZB", "CBOT", "rcal", "M23", "U23" ),
-    ( "ZN", "CBOT", "rcal", "M23", "U23" ),
-    ( "ZS", "CBOT", "custom", "-1:N23", "+1:Q23", "+1:U23", "-1:X23" )
+    ( "NG", "NYMEX", "rcal",  "X23", "H24" )
 ]
